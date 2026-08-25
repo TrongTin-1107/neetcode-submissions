911f7570fbd3a772fbb7e3e0723f25f59192a8f0
TrongTin-1107 <tinluong0811@gmail.com>
2026-08-25 09:29:50 +0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int res=0, min1 =1e5;
+        for(int i=0; i<prices.size(); i++){
+            if(prices[i]<min1) min1=prices[i];
+            int tmp=prices[i]-min1;
+            if(tmp>res){
+                res=tmp;
+            }
+        }
+        return res;
+   }
+};
